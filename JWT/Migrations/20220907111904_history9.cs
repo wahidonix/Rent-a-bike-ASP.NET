3bc0078290f3ceef22d285d42421b1204b203275
wahidonix <wahidonix@gmail.com>
2022-09-07 13:19:34 +0200

    add

---
+++ JWT/Migrations/20220907111904_history9.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JWT.Migrations
+{
+    public partial class history9 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Histories_Bikes_BikeId",
+                table: "Histories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Histories_BikeId",
+                table: "Histories");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Histories_BikeId",
+                table: "Histories",
+                column: "BikeId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Histories_Bikes_BikeId",
+                table: "Histories",
+                column: "BikeId",
+                principalTable: "Bikes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
